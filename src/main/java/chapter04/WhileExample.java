222917de8b747b2ff0e772fafbc3dd0c11a0c1cb
Cheshi Mantu <cheshi.mantu@protonmail.com>
2020-12-26 11:47:30 +0300

    while and do while

---
+++ src/main/java/chapter04/WhileExample.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+import static chapter03.ShortPrint.print;
+
+public class WhileExample {
+    static boolean condition(){
+        boolean result = Math.random() < 0.99;
+        print("" + result + ", ");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        while(condition())
+            print("Still inside while... ");
+        print("Exited while loop");
+    }
+}
+// do ... while will run at least once as the check is being performed at the end
