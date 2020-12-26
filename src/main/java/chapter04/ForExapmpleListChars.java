012ad4008d8a984e93283dd1b58cca7436966ded
Cheshi Mantu <cheshi.mantu@protonmail.com>
2020-12-26 12:39:30 +0300

    for loop example

---
+++ src/main/java/chapter04/ForExapmpleListChars.java
@@ -0,0 +1,14 @@
+package chapter04;
+
+import static chapter03.ShortPrint.print;
+
+public class ForExapmpleListChars {
+    public static void main(String[] args) {
+        for(char c = 0; c < 128; c++)
+            if(Character.isLowerCase(c))
+                print("Val: " + (int)c + " symbol: " + c);
+    for (int i = 0; i <=100; i++)
+        print(""+i);
+    }
+
+}
