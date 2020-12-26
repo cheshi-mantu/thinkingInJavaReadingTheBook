2fe23b44473aff02813a99ffcb9fbb36185cdd0f
Cheshi Mantu <cheshi.mantu@protonmail.com>
2020-12-26 16:55:45 +0300

    constructor and overloding

---
+++ src/main/java/chapter05/ConstructorAndInit.java
@@ -0,0 +1,25 @@
+package chapter05;
+
+import static chapter03.ShortPrint.print;
+
+/** Constructor and constructor overloading example
+ *
+ */
+class Rock {
+    String str;
+    Rock() { // Constructor method
+        print("Rock created with no name");
+        }
+    Rock(String strInit) { // Constructor method- overload
+        print("Rock created with the name " + strInit);
+        }
+}
+public class ConstructorAndInit {
+    public static void main(String[] args) {
+        for (int i = 0; i <=10; i++) {
+            new Rock();
+            new Rock("" + i);
+        }
+    }
+
+}
